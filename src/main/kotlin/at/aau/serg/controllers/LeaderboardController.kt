fe5ac0be4a4d0600418bcd771e9441a6c66ba236
Chris <Chris@DESKTOP-8D2BEIC>
2026-03-15 16:23:37 +0100

    LeaderboardController Code Kommentare ergänzt

--- src/main/kotlin/at/aau/serg/controllers/LeaderboardController.kt
+++ src/main/kotlin/at/aau/serg/controllers/LeaderboardController.kt
@@ -17,22 +17,46 @@ class LeaderboardController(
 
     @GetMapping
     fun getLeaderboard(@RequestParam(required = false) rank: Int?): List<GameResult> {
+        // Holt die gespeicherten Spielergebnisse vom Service
+
         val sortedResults = gameResultService.getGameResults()
             .sortedWith(compareByDescending<GameResult> { it.score }.thenBy { it.timeInSeconds })
+        //Score wird absteigend verglichen, höherer Score weiter oben, niedriger Score unten
+        //wenn gleicher Score, dann wird die bessere Zeit, also die kleinere Zeit genommen
 
         if (rank == null) {
             return sortedResults
         }
+        // Wenn kein rank-Parameter übergeben wurde,
+        // wird einfach das komplette Leaderboard zurückgegeben
 
         if (rank < 1 || rank > sortedResults.size) {
             throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid rank")
         }
+        // Überprüfung auf ungültige Rank-Werte
+        // Rank muss mindestens 1 sein und darf nicht größer sein als die Anzahl der Einträge
+
 
         val index = rank - 1
+        // Listen in Kotlin beginnen bei Index 0,
+        // während die Rangliste bei 1 beginnt.
+        // Deshalb wird der Rang auf einen Listenindex umgerechnet.
+
+
         val fromIndex = maxOf(0, index - 3)
+        //Berechnet den Startindex der Teilmenge.
+        // Es sollen bis zu 3 Spieler vor dem gewünschten Rang angezeigt werden.
+        // maxOf verhindert negative Indizes am Anfang der Liste.
+
         val toIndex = minOf(sortedResults.size, index + 4)
+        // Berechnet den Endindex der Teilmenge.
+        // Es sollen bis zu 3 Spieler nach dem gewünschten Rang angezeigt werden.
+        // minOf verhindert, dass über das Ende der Liste hinausgegriffen wird.
+
 
         return sortedResults.subList(fromIndex, toIndex)
+        // Gibt nur den berechneten Teilbereich des Leaderboards zurück.
+        // subList(start, end) enthält start (inklusive) bis end (exklusive).
     }
 
 }
